14d8b463d96d40f581f88b736432983f6485d0a1
Leonard Sean <seanchuatech@gmail.com>
2022-07-17 17:56:13 +0800

    test

--- 2-globals.js
+++ 2-globals.js
@@ -1,8 +1,9 @@
-const john = 'john';
-const peter = 'peter';
-
-const sayHi = (name) => {
-    console.log(`hello there ${name}`);
-}
-
-sayHi(peter);
+// CommonJS, every file is module (by default)
+// Modules - Encapsulated Code (only share minimum)
+const names = require('./04-names')
+const sayHi = require('./05-utils')
+const data = require('./06-alternative-flavor')
+require('./07-mind-grenade')
+sayHi('susan')
+sayHi(names.john)
+sayHi(names.peter)
--- app.js
+++ app.js
@@ -1,15 +1,6 @@
-const http = require('http');
+const _ = require('lodash');
 
-const server = http.createServer((req, res)=>{
-    if(req.url === '/'){
-        res.end('Welcome to our home page');
-    } else if (req.url === '/about') {
-        res.end('This is an about page');
-    } else {
-        res.end(`
-        <h1>Oops!</h1>
-        `)
-    }
-})
+const items = [1, [2, [3, [4]]]];
 
-server.listen(5000)
+const newItems = _.flattenDeep(items);
+console.log(newItems);
